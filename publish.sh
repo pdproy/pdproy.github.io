f386533ec68552cda5bc2c2e17ffc26bf33aa9cb
pradeep roy <pradeep.roy@gmail.com>
2025-01-18 18:02:12 +0530

    Organize into multiple directories (take 2)

--- publish.sh
+++ publish.sh
@@ -15,6 +15,9 @@ info() {
 update_links() {
     sed -i -e "s|\(link rel=\"stylesheet\" href=\)\".*\"|\1\"${CSS_URL}\"|" $1/*.html
     sed -i -e 's|\(<a href=.*\)\.adoc"|\1.html"|' $1/*.html
+
+    sed -i -e "s|\(link rel=\"stylesheet\" href=\)\".*\"|\1\"${CSS_URL}\"|" $1/*/*.html
+    sed -i -e 's|\(<a href=.*\)\.adoc"|\1.html"|' $1/*/*.html
 }
 
 git_checkout() {
@@ -25,10 +28,13 @@ main() {
     git_checkout "dev"
 
     info "Converting AsciiDoc files to HTML..."
-    mkdir -p $OUTPUT_DIR
+    mkdir -p ${OUTPUT_DIR}
     docker run   -u $(id -u):$(id -g) --rm -v .:/documents \
         asciidoctor/docker-asciidoctor \
-        asciidoctor -D $OUTPUT_DIR $SRC_DIR/*.adoc
+        asciidoctor ${SRC_DIR}/*.adoc ${SRC_DIR}/*/*.adoc
+
+    rsync -a ${SRC_DIR}/ ${OUTPUT_DIR}/
+    find ${OUTPUT_DIR} -type f ! -name '*.html' -delete
 
     info "Switching to gh-pages branch"
     git_checkout "gh-pages"
@@ -38,10 +44,10 @@ main() {
     info "Updating docs directory"
     rm -rf $DOCS_DIR/*
     mkdir -p ${DOCS_DIR}
-    cp -r $OUTPUT_DIR/* $DOCS_DIR/
+    cp -r ${OUTPUT_DIR}/* ${DOCS_DIR}/
 
     info "Committing and pushing changes"
-    git add $DOCS_DIR
+    git add ${DOCS_DIR}
     git commit -q -m "Update published site"
     git push origin gh-pages
 
